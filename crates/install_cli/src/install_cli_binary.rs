dc49c75665cb47486febd861d1abf8e892ce2127
Bing-Chang Lai <johnny.lai@me.com>
2026-05-09 08:06:22 -0700

    [Zetty Rename] Install zetty CLI instead of zed CLI

--- crates/install_cli/src/install_cli_binary.rs
+++ crates/install_cli/src/install_cli_binary.rs
@@ -25,7 +25,7 @@ const CANT_INSTALL_DOCS_URL: &str = "https://zed.dev/docs/macos#cant-install-cli
 /// commonly because the user is not an admin.
 async fn install_script(cx: &AsyncApp) -> Result<Option<PathBuf>> {
     let cli_path = cx.update(|cx| cx.path_for_auxiliary_executable("cli"))?;
-    let link_path = Path::new("/usr/local/bin/zed");
+    let link_path = Path::new("/usr/local/bin/zetty");
     let bin_dir_path = link_path.parent().unwrap();
 
     // Don't re-create symlink if it points to the same CLI binary.
